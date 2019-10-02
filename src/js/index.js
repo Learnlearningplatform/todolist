64973ae4cb2765b814a3f4a6f95675efd46b0a3d
TeAmP0is0N <rohitrathore.imh55@gmail.com>
2019-10-02 12:10:06 +0530

    Added path

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import 'bootstrap';
 
 //include your index.scss file into the bundle
 import '../styles/index.scss';
+import 'to do.html';
 
 //import your own components
 import {Home} from './component/home.jsx';
